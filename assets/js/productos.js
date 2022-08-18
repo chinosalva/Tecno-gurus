ef495356318059d56443aa660125fca366c536f4
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-08-18 02:09:19 -0500

    Casi finalizado codigo productos

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -4,3 +4,123 @@ planta1.addEventListener('click', function(e) {
   e.preventDefault
   modalPlanta1.classList.add('modal1--show')
 })
+const planta2 = document.getElementById('planta2')
+const modalPlanta2 = document.querySelector('.modal2')
+planta2.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta2.classList.add('modal1--show')
+})
+const planta3 = document.getElementById('planta3')
+const modalPlanta3 = document.querySelector('.modal3')
+planta3.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta3.classList.add('modal1--show')
+})
+const planta4 = document.getElementById('planta4')
+const modalPlanta4 = document.querySelector('.modal4')
+planta4.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta4.classList.add('modal1--show')
+})
+const planta5 = document.getElementById('planta5')
+const modalPlanta5 = document.querySelector('.modal5')
+planta5.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta5.classList.add('modal1--show')
+})
+const planta6 = document.getElementById('planta6')
+const modalPlanta6 = document.querySelector('.modal6')
+planta6.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta6.classList.add('modal1--show')
+})
+const planta7 = document.getElementById('planta7')
+const modalPlanta7 = document.querySelector('.modal7')
+planta7.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta7.classList.add('modal1--show')
+})
+const planta8 = document.getElementById('planta8')
+const modalPlanta8 = document.querySelector('.modal8')
+planta8.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta8.classList.add('modal1--show')
+})
+const planta9 = document.getElementById('planta9')
+const modalPlanta9 = document.querySelector('.modal9')
+planta9.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta9.classList.add('modal1--show')
+})
+const planta10 = document.getElementById('planta10')
+const modalPlanta10 = document.querySelector('.modal10')
+planta10.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta10.classList.add('modal1--show')
+})
+const planta11 = document.getElementById('planta11')
+const modalPlanta11 = document.querySelector('.modal11')
+planta11.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta11.classList.add('modal1--show')
+})
+const planta12 = document.getElementById('planta12')
+const modalPlanta12 = document.querySelector('.modal12')
+planta12.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta12.classList.add('modal1--show')
+})
+const planta13 = document.getElementById('planta13')
+const modalPlanta13 = document.querySelector('.modal13')
+planta13.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta13.classList.add('modal1--show')
+})
+const planta14 = document.getElementById('planta14')
+const modalPlanta14 = document.querySelector('.modal14')
+planta14.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta14.classList.add('modal1--show')
+})
+const planta15 = document.getElementById('planta15')
+const modalPlanta15 = document.querySelector('.modal15')
+planta15.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta15.classList.add('modal1--show')
+})
+const planta16 = document.getElementById('planta16')
+const modalPlanta16 = document.querySelector('.modal16')
+planta16.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta16.classList.add('modal1--show')
+})
+const planta17 = document.getElementById('planta17')
+const modalPlanta17 = document.querySelector('.modal17')
+planta17.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta17.classList.add('modal1--show')
+})
+const planta18 = document.getElementById('planta18')
+const modalPlanta18 = document.querySelector('.modal18')
+planta18.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta18.classList.add('modal1--show')
+})
+const planta19 = document.getElementById('planta19')
+const modalPlanta19 = document.querySelector('.modal19')
+planta19.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta19.classList.add('modal1--show')
+})
+const planta20 = document.getElementById('planta20')
+const modalPlanta20 = document.querySelector('.modal20')
+planta20.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta20.classList.add('modal1--show')
+})
+const planta21 = document.getElementById('planta21')
+const modalPlanta21 = document.querySelector('.modal21')
+planta21.addEventListener('click', function(e) {
+  e.preventDefault
+  modalPlanta21.classList.add('modal1--show')
+})
