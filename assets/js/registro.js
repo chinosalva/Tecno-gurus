42965cebf43265b6d16a2149131eb5ad1b9bdf8d
Alineo <alina.ortiz93@gmail.com>
2022-09-08 22:44:17 -0500

    Se realizó validación formularios

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,4 +1,6 @@
+const nombre = document.getElementById('exampleInputName')
 const email = document.getElementById('exampleInputEmail')
+const telefono = document.getElementById('exampleInputTelefono')
 const contraseña = document.getElementById('exampleInputPassword1')
 const formulario = document.getElementById('form')
 const advertencias = document.getElementById('advertencia')
@@ -6,8 +8,30 @@ const test = document.getElementById('test')
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault()
+    var expRegNombre=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
     var expRegContraseña= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
-    var expRegCorreo=/^\w+@(\w+\.)+\w{2,4}$/; 
+    var expRegCorreo=/^\w+@(\w+\.)+\w{2,4}$/;  
+    const buscarTelefono = /\d{10}/;
+
+       //Campo nombre
+    if(!nombre.value)
+    {
+        alert("El campo nombre es requerido");
+        nombre.focus();
+        return false;
+    }
+    if (!expRegNombre.exec(nombre.value) || nombre.value.length < 22)
+    {
+        advertencias.innerHTML = 'El campo nombre debe contar con nombre y apellido'
+        nombre.focus();
+        return false;
+    }
+
+    if (expRegNombre.exec(nombre.value) || nombre.value.length > 22)
+    {
+        advertencias.innerHTML = ''
+    }
+
 
        //campo email
     if(!email.value)
@@ -23,6 +47,23 @@ formulario.addEventListener('submit', (e) => {
         return false;
     }
     if(expRegCorreo.exec(email.value))
+    {
+        advertencias.innerHTML = ''
+    }
+       // campo telefono
+    if(!telefono.value)
+    {
+        alert("El campo teléfono es requerido");
+        telefono.focus();
+        return false;
+    }
+    if(!buscarTelefono.exec(telefono.value))
+    {
+        advertencias.innerHTML = 'El campo teléfono debe incluir al menos 10 dígitos'
+        telefono.focus();
+        return false;
+    }
+    if(buscarTelefono.exec(telefono.value))
     {
         advertencias.innerHTML = ''
     }
@@ -44,7 +85,8 @@ formulario.addEventListener('submit', (e) => {
     {
         advertencias.innerHTML = ''
     }
-    alert('¡Felicidades has ingresado con éxito!');
+    
+    alert('¡Felicidades has ingresado con éxito!')
     return true;
     }
 )
